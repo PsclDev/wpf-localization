c177335792f48c87a6165784266dab2c4e12ba3a
Pascal <github@unrundead.eu>
2019-05-10 21:53:12 +0200

    Added LanguageFile for new UserControl

---
+++ Localization/UserControl_Test.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Localization
+{
+    /// <summary>
+    /// Interaction logic for UserControl_Test.xaml
+    /// </summary>
+    public partial class UserControl_Test : UserControl
+    {
+        //private MainWindow mw = new MainWindow();
+
+        public UserControl_Test()
+        {
+            InitializeComponent();
+        }
+
+        public void ChangeLanguage()
+        {
+        }
+    }
+}
